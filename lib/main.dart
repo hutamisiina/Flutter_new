5e64b35f06e2824e1c8706b846b31540d6027bf3
hutamisiina <genzsk1210@gmail.com>
2025-05-05 01:53:30 +0900

    new

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,10 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -38,8 +42,10 @@ class MyHomePage extends StatelessWidget {
       body: Column(
         children: [
           Text('わにゃちゃん！:'),
+          Text(appState.current.asLowerCase),
           ElevatedButton(
             onPressed: () {
+              appState.getNext();
               print('わにゃちゃん pressed!');
             },
             child: Text('わにゃわにゃわ'),
